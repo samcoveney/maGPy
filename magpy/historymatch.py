ce6a6f66f67c8a6b36437d07cd560c422d629f83
samcoveney <coveney.sam@gmail.com>
2017-08-23 16:29:15 +0100

    Updated history matching

--- magpy/historymatch.py
+++ magpy/historymatch.py
@@ -35,7 +35,7 @@ class Wave:
     ## pickle a list of relevant data
     def save(self, filename):
         print("Pickling wave data in", filename, "...")
-        w = [ self.TESTS, self.I, self.NIMP, self.NIMP_I ]  # test these 3 for now
+        w = [ self.TESTS, self.I, self.NIMP, self.NIMP_I, self.doneImp ]  # test these 3 for now
         with open(filename, 'wb') as output:
             pickle.dump(w, output, pickle.HIGHEST_PROTOCOL)
         return
@@ -45,7 +45,7 @@ class Wave:
         print("Unpickling wave data in", filename, "...")
         with open(filename, 'rb') as input:
             w = pickle.load(input)
-        self.TESTS, self.I, self.NIMP, self.NIMP_I = w[0], w[1], w[2], w[3]
+        self.TESTS, self.I, self.NIMP, self.NIMP_I, self.doneImp = w[0], w[1], w[2], w[3], w[4]
         return
 
     ## set the test data
@@ -63,7 +63,7 @@ class Wave:
             #self.TESTS = xTemp.astype(np.float16)
 
             self.TESTS = tests.astype(np.float16)
-            self.I = np.empty((self.TESTS[:,0].size,len(self.emuls)),dtype=np.float16)
+            self.I = np.empty((self.TESTS.shape[0],len(self.emuls)),dtype=np.float16)
         else:
             print("ERROR: tests must be a numpy array")
         return
@@ -126,29 +126,28 @@ def myGrey():
     return '#696988'
 
 def impcolormap():
+    #return colors.LinearSegmentedColormap.from_list('imp',
+    #  [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.80, '#e2721b'), (1, '#db0100')], N=256)
     return colors.LinearSegmentedColormap.from_list('imp',
-      [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.80, '#e2721b'), (1, '#db0100')], N=256)
+      [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.95, '#e2721b'), (1, '#db0100')], N=1000)
 
 def odpcolormap():
-    return colors.LinearSegmentedColormap.from_list('odp',
-      [(0, myGrey()), (1.0/float(256), '#ffffff'),
+#    return colors.LinearSegmentedColormap.from_list('odp',
+#      [(0, myGrey()), (0.00000001, '#ffffff'),
+#       (0.20, '#93ffff'), (0.45, '#5190fc'), (0.65, '#0000fa'), (1, '#db00fa')], N=1000)
+    cmap = colors.LinearSegmentedColormap.from_list('odp',
+      [(0.0, '#ffffff'),
        (0.20, '#93ffff'), (0.45, '#5190fc'), (0.65, '#0000fa'), (1, '#db00fa')], N=256)
+    cmap.set_under(color=myGrey())
+    return cmap
 
 
 ## implausibility and optical depth plots for all pairs of active indices
-def plotImp(waves, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename="hexbin.pkl"):
-
-    print("HM plotting. Determining max", maxno,"imps...")
-
-    wave = waves
-    TESTS = wave.TESTS
-    P = TESTS[:,0].size
-    Imaxes = np.array( [np.sort(np.partition(wave.I[r,:],-maxno)[-maxno:])[-maxno]
-                         for r in range(P)] )
+def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename="hexbin.pkl", points=[], replot=False):
 
     ## make list of all the active indices across all emulators
     active = []
-    for e in waves.emuls:
+    for e in wave.emuls:
         for a in e.Data.active:
             if a not in active: active.append(a)
     active.sort()
@@ -161,15 +160,6 @@ def plotImp(waves, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filenam
 
     minmax = wave.emuls[0].Data.minmax
 
-    ## space for all plots, and reference index to subplot indices
-    print("Creating HM plot objects...")
-    rc = len(active)
-    fig, ax = plt.subplots(nrows = rc, ncols = rc)
-
-    ## set colorbar bounds
-    impCB = [0, wave.cm] if impCB == [] else impCB
-    odpCB = [0, 1] if odpCB == [] else odpCB
-
     ## create list of all pairs of active inputs
     gSets = []
     for i in active:
@@ -177,69 +167,75 @@ def plotImp(waves, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filenam
             if i!=j and i<j and [i,j] not in gSets:  gSets.append([i,j])
     print("GLOBAL SETS:", gSets)
 
-    print("Making subplots of paired indices...")
-    ## loop over plot_bins()
-    for s in gSets:
-        ## minmax is always [0,1] now, so don't need this
-        #ex = ( minmax[s[0]][0], minmax[s[0]][1], minmax[s[1]][0], minmax[s[1]][1])
+    if replot == False:
 
-        ax[pltRef[s[1]],pltRef[s[0]]].patch.set_facecolor(myGrey())
-        im_imp = ax[pltRef[s[1]],pltRef[s[0]]].hexbin(
-          TESTS[:,s[0]], TESTS[:,s[1]], C = Imaxes,
-          gridsize=grid, cmap=impcolormap(), vmin=impCB[0], vmax=impCB[1],
-          #extent=ex,
-          reduce_C_function=np.min, linewidths=linewidths, mincnt=1)
+        ## determine the max'th Implausibility
+        print("Determining", maxno, "max'th implausibility...")
+        Imaxes = np.array( [np.sort(np.partition(wave.I[r,:],-maxno)[-maxno:])[-maxno]
+                         for r in range(wave.TESTS.shape[0])] )
 
-        ax[pltRef[s[0]],pltRef[s[1]]].patch.set_facecolor(myGrey())
-        im_odp = ax[pltRef[s[0]],pltRef[s[1]]].hexbin(
-          TESTS[:,s[0]], TESTS[:,s[1]], C = Imaxes<wave.cm,
-          gridsize=grid, cmap=odpcolormap(), vmin=odpCB[0], vmax=odpCB[1],
-          #extent=ex,
-          linewidths=linewidths, mincnt=1)
+        ## space for all plots, and reference index to subplot indices
+        print("Creating HM plot objects...")
+        rc = len(active)
+        fig, ax = plt.subplots(nrows = rc, ncols = rc)
 
-        plt.colorbar(im_imp, ax=ax[pltRef[s[1]],pltRef[s[0]]])
-        plt.colorbar(im_odp, ax=ax[pltRef[s[0]],pltRef[s[1]]])
+        ## set colorbar bounds
+        impCB = [0, wave.cm] if impCB == [] else impCB
+        odpCB = [0.00000001, None] if odpCB == [] else odpCB
+        #odpCB = [None, None] if odpCB == [] else odpCB
 
-    ## some plot options
-    for a in range(rc):  fig.delaxes(ax[a,a])
-    for a in ax.flat:
-        a.set(adjustable='box-forced', aspect='equal')
-        a.set_xticks([]); a.set_yticks([]); a.set_aspect('equal')
+        print("Making subplots of paired indices...")
+        ## loop over plot_bins()
+        printProgBar(0, len(gSets), prefix = 'Progress:', suffix = '')
+        for i, s in enumerate(gSets):
+            ## minmax is always [0,1] now, so don't need this
+            #ex = ( minmax[s[0]][0], minmax[s[0]][1], minmax[s[1]][0], minmax[s[1]][1])
+            ex = ( 0,1,0,1 )
 
-    plt.tight_layout()
+            ax[pltRef[s[1]],pltRef[s[0]]].patch.set_facecolor(myGrey())
+            im_imp = ax[pltRef[s[1]],pltRef[s[0]]].hexbin(
+              wave.TESTS[:,s[0]], wave.TESTS[:,s[1]], C = Imaxes,
+              gridsize=grid, cmap=impcolormap(), vmin=impCB[0], vmax=impCB[1],
+              extent=ex,
+              reduce_C_function=np.min, linewidths=linewidths, mincnt=1)
+
+            ax[pltRef[s[0]],pltRef[s[1]]].patch.set_facecolor(myGrey())
+            im_odp = ax[pltRef[s[0]],pltRef[s[1]]].hexbin(
+              wave.TESTS[:,s[0]], wave.TESTS[:,s[1]],
+              C = Imaxes<wave.cm,
+              #gridsize=grid, bins='log', cmap=odpcolormap(), vmin=odpCB[0], vmax=odpCB[1],
+              gridsize=grid, cmap=odpcolormap(), vmin=odpCB[0], vmax=odpCB[1],
+              extent=ex,
+              linewidths=linewidths, mincnt=1)
+
+            plt.colorbar(im_imp, ax=ax[pltRef[s[1]],pltRef[s[0]]])
+            plt.colorbar(im_odp, ax=ax[pltRef[s[0]],pltRef[s[1]]])
+
+            printProgBar(i+1, len(gSets), prefix = 'Progress:', suffix = '')
+
+        ## some plot options
+        for a in range(rc):  fig.delaxes(ax[a,a])
+        for a in ax.flat:
+            a.set(adjustable='box-forced', aspect='equal')
+            a.set_xticks([]); a.set_yticks([]); a.set_aspect('equal')
+
+        plt.tight_layout()
+
+    else:
+        fig, ax = pickle.load(open(filename,'rb'))  # load plot
+
+    ## plots points
+    if points is not []:
+        for p in points:
+            for s in gSets:
+                ax[s[1],s[0]].scatter(p[s[0]], p[s[1]], s=15, c='black')
+                ax[s[0],s[1]].scatter(p[s[0]], p[s[1]], s=15, c='black')
+
     
-    pickle.dump([fig, ax, impCB], open(filename, 'wb'))  # save plot
+    pickle.dump([fig, ax], open(filename, 'wb'))  # save plot
     # This is for Python 3 - py2 may need `file` instead of `open`
 
     plt.show()
     return
 
 
-## replot imp/odp using pickle file
-def replot_imps(filename="hexbin.pkl", points=[], Is=[]):
-
-    fig, ax, impCB = pickle.load(open(filename,'rb'))
-
-    if points != []:
-        sets = []
-        p = points[:,0].size
-        dim = points[0,:].size
-        for i in range(dim):
-            for j in range(dim):
-                if i!=j and i<j and [i,j] not in sets:
-                    sets.append([i,j])
-        #print("SETS:", sets)
-
-        ## for visualising new wave sim inputs, there will be an option to plot points
-        for s in sets:
-            for i in range(p):        
-                ax[s[1],s[0]].scatter(points[i,s[0]], points[i,s[1]], s=15, c='black')
-                ax[s[0],s[1]].scatter(points[i,s[0]], points[i,s[1]], s=15, c='black')
-                #ax[s[1],s[0]].scatter(points[i,s[0]], points[i,s[1]], s=20,\
-                #        c=Is[i], cmap=impcolormap(), vmin=impCB[0], vmax=impCB[1])
-                #ax[s[0],s[1]].scatter(points[i,s[0]], points[i,s[1]], s=20,\
-                #        c=Is[i], cmap=impcolormap(), vmin=impCB[0], vmax=impCB[1])
-
-    plt.show()
-
-    return
