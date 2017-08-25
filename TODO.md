1b31c972cbce1dfe5436bdadd8cda734b5d956f1
samcoveney <coveney.sam@gmail.com>
2017-08-25 14:15:21 +0100

    Improved HM plots and sped-up routines

--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 
 ## general
 * check how the nugget should be used for prediction vs estimation with the posterior
+* provide option to use a constant scalar mean
+
+## history matching
--- magpy/historymatch.py
+++ magpy/historymatch.py
@@ -21,21 +21,19 @@ class Wave:
         ## passed in
         self.emuls = emuls
         self.zs, self.var, self.cm = zs, var, cm
+        self.I = []
+        self.doneImp = False
         if tests is not []:
             self.setTests(tests)
 
-        self.doneImp = False
-
-        ## could replace these with indices into the test points, rather than store more
-        self.NIMP = []  # for storing all found imp points (index into test_points..?)
-        self.NIMP_I = []  # for storing all found imp points imp values
+        self.NIMP = []  # for storing indices of TESTS with Im < cm 
         self.NROY = []  # create a design to fill NROY space based of found NIMP points
 
 
     ## pickle a list of relevant data
     def save(self, filename):
         print("Pickling wave data in", filename, "...")
-        w = [ self.TESTS, self.I, self.NIMP, self.NIMP_I, self.doneImp ]  # test these 3 for now
+        w = [ self.TESTS, self.I, self.NIMP, self.doneImp ]  # test these 3 for now
         with open(filename, 'wb') as output:
             pickle.dump(w, output, pickle.HIGHEST_PROTOCOL)
         return
@@ -45,23 +43,12 @@ class Wave:
         print("Unpickling wave data in", filename, "...")
         with open(filename, 'rb') as input:
             w = pickle.load(input)
-        self.TESTS, self.I, self.NIMP, self.NIMP_I, self.doneImp = w[0], w[1], w[2], w[3], w[4]
+        self.TESTS, self.I, self.NIMP, self.doneImp = [i for i in w]
         return
 
     ## set the test data
     def setTests(self, tests):
-
         if isinstance(tests, np.ndarray):
-
-            ## WHY SCALE - JUST PROVIDE UNIT HYPERCUBE OF TESTS???
-            ## scale the inputs
-            #E = self.emuls[0]
-            #xTemp = np.empty(tests.shape)
-            #for i in range(tests.shape[1]):
-            #    xTemp[:,i] = ( tests[:,i] - E.Data.minmax[i][0] ) \
-            #               / ( E.Data.minmax[i][1] - E.Data.minmax[i][0] )
-            #self.TESTS = xTemp.astype(np.float16)
-
             self.TESTS = tests.astype(np.float16)
             self.I = np.empty((self.TESTS.shape[0],len(self.emuls)),dtype=np.float16)
         else:
@@ -75,10 +62,11 @@ class Wave:
         P = self.TESTS[:,0].size
         if P > chunkSize:
             chunkNum = int(np.ceil(P / chunkSize))
-            print("\nCalculating Implausibilities of", P , "points in", chunkNum, "chunks of", chunkSize)
+            print("\nCalculating Implausibilities of", P, "points in",\
+                    chunkNum, "chunks of", chunkSize)
         else:
             chunkNum = 1
-            print("\nCalculating Implausibilities of", P , "points")
+            print("\nCalculating Implausibilities of", P, "points")
 
         ## loop over outputs (i.e. over emulators)
         printProgBar(0, len(self.emuls)*chunkNum, prefix = 'Progress:', suffix = '')
@@ -100,22 +88,17 @@ class Wave:
     ## find all the non-implausible points in the test points
     def findNIMP(self, maxno=1):
 
-        self.NIMP, self.NIMP_I = [], []  # make empty because may call twice (different maxnos)
-
         if self.doneImp == False:
             print("WARNING: implausibilities must first be calculated with calcImp()")
             return
 
         P = self.TESTS[:,0].size
-        for r in range(P):
-            ## find maximum implausibility across different outputs
-            Imaxes = np.sort(np.partition(self.I[r,:],-maxno)[-maxno:])[-maxno:]
-            ## check cut-off
-            if Imaxes[-(maxno)] < self.cm:
-                self.NIMP.append(self.TESTS[r])
-                self.NIMP_I.append(Imaxes)
+        ## find maximum implausibility across different outputs
+        print("Determining", maxno, "max'th implausibility...")
+        Imaxes = np.partition(self.I, -maxno)[:,-maxno]
 
-        self.NIMP, self.NIMP_I = np.asarray(self.NIMP), np.asarray(self.NIMP_I)
+        ## check cut-off, store indices of points matching condition
+        self.NIMP = np.argwhere(Imaxes < self.cm)
         print("NIMP fraction:", 100*float(len(self.NIMP))/float(P), "%")
 
         return
@@ -126,15 +109,11 @@ def myGrey():
     return '#696988'
 
 def impcolormap():
-    #return colors.LinearSegmentedColormap.from_list('imp',
-    #  [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.80, '#e2721b'), (1, '#db0100')], N=256)
     return colors.LinearSegmentedColormap.from_list('imp',
-      [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.95, '#e2721b'), (1, '#db0100')], N=1000)
+      [(0, '#90ff3c'), (0.50, '#ffff3c'), (0.95, '#e2721b'), (1, '#db0100')], N=256)
 
 def odpcolormap():
-#    return colors.LinearSegmentedColormap.from_list('odp',
-#      [(0, myGrey()), (0.00000001, '#ffffff'),
-#       (0.20, '#93ffff'), (0.45, '#5190fc'), (0.65, '#0000fa'), (1, '#db00fa')], N=1000)
+    #[(0, myGrey()), (0.00000001, '#ffffff'),  # alternative first colours
     cmap = colors.LinearSegmentedColormap.from_list('odp',
       [(0.0, '#ffffff'),
        (0.20, '#93ffff'), (0.45, '#5190fc'), (0.65, '#0000fa'), (1, '#db00fa')], N=256)
@@ -143,7 +122,7 @@ def odpcolormap():
 
 
 ## implausibility and optical depth plots for all pairs of active indices
-def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename="hexbin.pkl", points=[], replot=False):
+def plotImp(wave, maxno=1, grid=10, impMax=None, odpMax=None, linewidths=0.2, filename="hexbin.pkl", points=[], replot=False, globalColorbar=False):
 
     ## make list of all the active indices across all emulators
     active = []
@@ -171,8 +150,7 @@ def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename
 
         ## determine the max'th Implausibility
         print("Determining", maxno, "max'th implausibility...")
-        Imaxes = np.array( [np.sort(np.partition(wave.I[r,:],-maxno)[-maxno:])[-maxno]
-                         for r in range(wave.TESTS.shape[0])] )
+        Imaxes = np.partition(wave.I, -maxno)[:,-maxno]
 
         ## space for all plots, and reference index to subplot indices
         print("Creating HM plot objects...")
@@ -180,8 +158,11 @@ def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename
         fig, ax = plt.subplots(nrows = rc, ncols = rc)
 
         ## set colorbar bounds
-        impCB = [0, wave.cm] if impCB == [] else impCB
-        odpCB = [0.00000001, None] if odpCB == [] else odpCB
+        impCB = [0, wave.cm] if impMax == None else [0.0, impMax]
+        if odpMax == None and globalColorbar == True:
+            print("WARNING: cannot use globalColorbar without specifying odpMax for ODP colorbar")
+            globalColorbar = False
+        odpCB = [0.00000001, None] if odpMax == None else [0.00000001, odpMax]
         #odpCB = [None, None] if odpCB == [] else odpCB
 
         print("Making subplots of paired indices...")
@@ -192,15 +173,17 @@ def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename
             #ex = ( minmax[s[0]][0], minmax[s[0]][1], minmax[s[1]][0], minmax[s[1]][1])
             ex = ( 0,1,0,1 )
 
-            ax[pltRef[s[1]],pltRef[s[0]]].patch.set_facecolor(myGrey())
-            im_imp = ax[pltRef[s[1]],pltRef[s[0]]].hexbin(
+            impPlot, odpPlot = ax[pltRef[s[1]],pltRef[s[0]]], ax[pltRef[s[0]],pltRef[s[1]]]
+
+            impPlot.patch.set_facecolor(myGrey())
+            im_imp = impPlot.hexbin(
               wave.TESTS[:,s[0]], wave.TESTS[:,s[1]], C = Imaxes,
               gridsize=grid, cmap=impcolormap(), vmin=impCB[0], vmax=impCB[1],
               extent=ex,
               reduce_C_function=np.min, linewidths=linewidths, mincnt=1)
 
-            ax[pltRef[s[0]],pltRef[s[1]]].patch.set_facecolor(myGrey())
-            im_odp = ax[pltRef[s[0]],pltRef[s[1]]].hexbin(
+            odpPlot.patch.set_facecolor(myGrey())
+            im_odp = odpPlot.hexbin(
               wave.TESTS[:,s[0]], wave.TESTS[:,s[1]],
               C = Imaxes<wave.cm,
               #gridsize=grid, bins='log', cmap=odpcolormap(), vmin=odpCB[0], vmax=odpCB[1],
@@ -208,16 +191,39 @@ def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename
               extent=ex,
               linewidths=linewidths, mincnt=1)
 
-            plt.colorbar(im_imp, ax=ax[pltRef[s[1]],pltRef[s[0]]])
-            plt.colorbar(im_odp, ax=ax[pltRef[s[0]],pltRef[s[1]]])
+            if globalColorbar == False:
+                plt.colorbar(im_imp, ax=impPlot)
+                plt.colorbar(im_odp, ax=odpPlot)
+
+            #impPlot.set_xlabel(minmax[s[0]])
+            xlabels = [item.get_text() for item in impPlot.get_xticklabels()]
+            ylabels = [item.get_text() for item in impPlot.get_yticklabels()]
+            xLabels, yLabels = ['']*len(xlabels), ['']*len(ylabels)
+            impPlot.set_xticklabels(xLabels); impPlot.set_yticklabels(yLabels)
+            odpPlot.set_xticklabels(xLabels); odpPlot.set_yticklabels(yLabels)
+            #impPlot.set_xlabel("sam") 
 
             printProgBar(i+1, len(gSets), prefix = 'Progress:', suffix = '')
 
+        ## global colorbars
+        if globalColorbar == True:
+            plt.draw()
+            p0 = ax[0,0].get_position().get_points().flatten()
+            p1 = ax[rc-1,rc-1].get_position().get_points().flatten()
+            # imp
+            ax_cbar = fig.add_axes([0.035, p0[0], 0.01, p1[2]-p0[0]])
+            plt.colorbar(im_imp, cax=ax_cbar)
+            # odp
+            ax_cbar = fig.add_axes([p1[2] + 0.045, p0[0], 0.01, p1[2]-p0[0]])
+            plt.colorbar(im_odp, cax=ax_cbar)
+
         ## some plot options
-        for a in range(rc):  fig.delaxes(ax[a,a])
+        for a in range(rc):
+            fig.delaxes(ax[a,a])
+
         for a in ax.flat:
             a.set(adjustable='box-forced', aspect='equal')
-            a.set_xticks([]); a.set_yticks([]); a.set_aspect('equal')
+            #a.set_xticks([]); a.set_yticks([]); a.set_aspect('equal')
 
         plt.tight_layout()
 
@@ -226,6 +232,7 @@ def plotImp(wave, maxno=1, grid=10, impCB=[], odpCB=[], linewidths=0.2, filename
 
     ## plots points
     if points is not []:
+        print("Plotting points as well...")
         for p in points:
             for s in gSets:
                 ax[s[1],s[0]].scatter(p[s[0]], p[s[1]], s=15, c='black')
